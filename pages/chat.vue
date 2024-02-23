0f3652118e682f7a6c17f44655dec5ebf20a373c
Alexander Tarling <tarlinga@cardiff.ac.uk>
2024-02-23 16:48:25 +0000

    Outputs JSON to chat page (for testing purposes), only if data is fetched correctly

--- pages/chat.vue
+++ pages/chat.vue
@@ -1,15 +1,15 @@
 <template>
     <div>
         <!--   used for testing json data fetched, delete soon   -->
-        <p>{{ threeQsJSON }}</p>
+        <span v-if="questions">JSON questions: {{ questions }}</span>
     </div>
 </template>
 
 <script setup>
 const config = useRuntimeConfig();
 // fetches a JSON array of 3 suggested questions
-// from /suggested in utils/suggested_questions.py
-const threeQsJSON = await useFetch(`${config.public.apiURL}/suggested`);
+// from suggested_questions.py
+const { data: questions } = await useFetch(`${config.public.apiURL}/suggested`);
 </script>
 
 <style></style>
