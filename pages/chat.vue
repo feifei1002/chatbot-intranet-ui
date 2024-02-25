a8a36034c8840cfc90e517aaac8c2b726540657f
Alexander Tarling <tarlinga@cardiff.ac.uk>
2024-02-25 14:40:25 +0000

    Removed suggested questions JSON array from being posted to the webpage, is printed to the console instead

--- pages/chat.vue
+++ pages/chat.vue
@@ -1,15 +1,15 @@
 <template>
-    <div>
-        <!--   used for testing json data fetched, delete soon   -->
-        <span v-if="questions">JSON questions: {{ questions }}</span>
-    </div>
+    <div></div>
 </template>
 
 <script setup>
+// get variables from nuxt.config.ts
 const config = useRuntimeConfig();
+
 // fetches a JSON array of 3 suggested questions
-// from suggested_questions.py
 const { data: questions } = await useFetch(`${config.public.apiURL}/suggested`);
+console.log("JSON array of suggested questions is:");
+console.log(questions);
 </script>
 
 <style></style>
