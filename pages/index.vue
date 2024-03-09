91f355616d4bc7ba45984fc99c1252139e28b012
Alexander Tarling <tarlinga@cardiff.ac.uk>
2024-03-09 19:34:02 +0000

    Fixed lint error

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template class="h-screen w-screen">
     <div class="flex">
         <div class="flex h-screen w-1/2 items-center justify-center bg-chatbot-black">
-            <p class="flex justify-around text-balance pt-6 text-center font-mono text-4xl font-bold text-chatbot-white">
+            <p
+                class="flex justify-around text-balance pt-6 text-center font-mono text-4xl font-bold text-chatbot-white"
+            >
                 This is a free-to-use chatbot that can return any information or answer any questions you may have
                 relating to your student life!
             </p>
