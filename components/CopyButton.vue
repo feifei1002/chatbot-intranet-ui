b799d8d5d692047298b369fd35128cdae026515d
c21077361 <vigneswaranj@cardiff.ac.uk>
2024-03-09 21:29:21 +0000

    I have implemented a copy button

---
+++ components/CopyButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <UButton
+        icon="i-heroicons-clipboard-document"
+        size="sm"
+        color="primary"
+        variant="solid"
+        label="Copy"
+        :trailing="false"
+        @click="copyText"
+    />
+</template>
+
+<script setup>
+const props = defineProps({
+    content: {
+        type: String,
+        default: "",
+    },
+});
+
+const copyText = () => {
+    const textarea = document.createElement("textarea");
+    textarea.value = props.content;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+
+    // Optionally, you can emit an event or perform any other action
+    console.log("Text copied:", props.content);
+};
+</script>
