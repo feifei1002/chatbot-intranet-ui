c1fbc235f609a04f801c219ce7d3bf10f3d8170b
Alexander Tarling <tarlinga@cardiff.ac.uk>
2024-03-17 15:25:55 +0000

    Fixed outputting i18n text to AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -10,23 +10,28 @@ watch(status, newStatus => {
     <nav class="bg-chatbot-white p-6">
         <div class="container flex items-center">
             <NuxtImg preload src="/img/logo.png" width="60" />
-            <NuxtLink to="/" class="w-3/4 pl-4 text-xl font-bold text-chatbot-font">Chatbot</NuxtLink>
+            <NuxtLink v-t="'titles.chatbot'" to="/" class="w-3/4 pl-4 text-xl font-bold text-chatbot-font" />
             <div class="absolute right-0 flex w-1/4 justify-between pr-6">
-                <NuxtLink to="/" class="text-xl font-semibold text-chatbot-font" exact-active-class="!text-chatbot-red"
-                    >Home</NuxtLink
+                <NuxtLink
+                    v-t="'titles.home'"
+                    to="/"
+                    class="text-xl font-semibold text-chatbot-font"
+                    exact-active-class="!text-chatbot-red"
+                />
                 >
                 <NuxtLink
                     v-if="!authStatus"
+                    v-t="'titles.signin'"
                     to="/signin"
                     class="text-xl font-semibold text-chatbot-font"
                     exact-active-class="!text-chatbot-red"
-                    >Sign In</NuxtLink
+                />
                 >
                 <NuxtLink
                     to="/chat"
                     class="text-xl font-semibold text-chatbot-font"
                     exact-active-class="!text-chatbot-red"
-                    >Try Chatbot
+                    >{{ $t("titles.trychatbot") }}
                     <UIcon name="i-heroicons-arrow-up-right" />
                 </NuxtLink>
             </div>
