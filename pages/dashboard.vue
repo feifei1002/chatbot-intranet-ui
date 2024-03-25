b2643557f203e2359863eea793fe8b56e3926f59
Dylan Silva <c21088964@cardiff.ac.uk>
2024-03-25 14:35:43 +0000

    Refactor dashboard layout with tailwind

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,25 +1,36 @@
 <template>
     <div>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-    </div>
-    <div>
-        <div>
-            <h1>Activity this hour</h1>
-            <div>Graph 1</div>
-            <div>Graph 2</div>
-            <div>Graph 3</div>
+        <!-- First set of divs -->
+        <div class="flex">
+            <div class="mr-4">1</div>
+            <div class="mr-4">2</div>
+            <div class="mr-4">3</div>
+            <div class="mr-4">4</div>
+            <div class="mr-4">5</div>
+            <div>6</div>
         </div>
-    </div>
-    <div>
-        <div>
-            <h1>Historical stats</h1>
-            <div>Graph 1</div>
-            <div>Graph 2</div>
+
+        <!-- Second set of divs -->
+        <div class="mt-4">
+            <div>
+                <h1>Activity this hour</h1>
+                <div class="flex">
+                    <div class="mr-4">Graph 1</div>
+                    <div class="mr-4">Graph 2</div>
+                    <div>Graph 3</div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Third set of divs -->
+        <div class="mt-4">
+            <div>
+                <h1>Historical stats</h1>
+                <div class="grid grid-cols-2 gap-4">
+                    <div>Graph 1</div>
+                    <div>Graph 2</div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
