f4bdd854b2f997d3b54fcc70eccebb65dce8b208
Dylan Silva <c21088964@cardiff.ac.uk>
2024-03-25 15:00:55 +0000

    all divs positioned properly

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -2,35 +2,37 @@
     <div class="flex justify-center">
         <!-- All content goes here -->
         <div>
-            <!-- First set of divs -->
-            <div class="flex">
-                <div class="mr-4">1</div>
-                <div class="mr-4">2</div>
-                <div class="mr-4">3</div>
-                <div class="mr-4">4</div>
-                <div class="mr-4">5</div>
-                <div>6</div>
-            </div>
+            <div>
+                <!-- First set of divs -->
+                <div class="mt-5 flex">
+                    <div class="mr-8 h-28 w-64 rounded border border-gray-300 p-12">1</div>
+                    <div class="mr-8 h-28 w-64 rounded border border-gray-300 p-12">2</div>
+                    <div class="mr-8 h-28 w-64 rounded border border-gray-300 p-12">3</div>
+                    <div class="mr-8 h-28 w-64 rounded border border-gray-300 p-12">4</div>
+                    <div class="mr-8 h-28 w-64 rounded border border-gray-300 p-12">5</div>
+                    <div class="h-28 w-64 rounded border border-gray-300 p-12">6</div>
+                </div>
 
-            <!-- Second set of divs -->
-            <div class="mt-4">
-                <div>
-                    <h1>Activity this hour</h1>
-                    <div class="flex">
-                        <div class="mr-4">Graph 1</div>
-                        <div class="mr-4">Graph 2</div>
-                        <div>Graph 3</div>
+                <!-- Second set of divs -->
+                <div class="mt-4">
+                    <div>
+                        <h1>Activity this hour</h1>
+                        <div class="flex">
+                            <div class="mr-4 h-20 w-64 grow rounded border border-gray-300 p-12">Graph 1</div>
+                            <div class="mr-4 h-20 w-64 grow rounded border border-gray-300 p-12">Graph 2</div>
+                            <div class="h-20 w-64 grow rounded border border-gray-300 p-12">Graph 3</div>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <!-- Third set of divs -->
-            <div class="mt-4">
-                <div>
-                    <h1>Historical stats</h1>
-                    <div class="grid grid-cols-2 gap-4">
-                        <div>Graph 1</div>
-                        <div>Graph 2</div>
+                <!-- Third set of divs -->
+                <div class="mt-4">
+                    <div>
+                        <h1>Historical stats</h1>
+                        <div class="flex">
+                            <div class="mr-4 h-20 w-64 grow rounded border border-gray-300 p-12">Graph 1</div>
+                            <div class="h-20 w-64 grow rounded border border-gray-300 p-12">Graph 2</div>
+                        </div>
                     </div>
                 </div>
             </div>
