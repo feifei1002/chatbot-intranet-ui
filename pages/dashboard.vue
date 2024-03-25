f3bb499f5286800b752d98f98ecdc26603dcd31e
Dylan Silva <c21088964@cardiff.ac.uk>
2024-03-25 14:38:03 +0000

    centred all divs

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,34 +1,37 @@
 <template>
-    <div>
-        <!-- First set of divs -->
-        <div class="flex">
-            <div class="mr-4">1</div>
-            <div class="mr-4">2</div>
-            <div class="mr-4">3</div>
-            <div class="mr-4">4</div>
-            <div class="mr-4">5</div>
-            <div>6</div>
-        </div>
+    <div class="flex justify-center">
+        <!-- All content goes here -->
+        <div>
+            <!-- First set of divs -->
+            <div class="flex">
+                <div class="mr-4">1</div>
+                <div class="mr-4">2</div>
+                <div class="mr-4">3</div>
+                <div class="mr-4">4</div>
+                <div class="mr-4">5</div>
+                <div>6</div>
+            </div>
 
-        <!-- Second set of divs -->
-        <div class="mt-4">
-            <div>
-                <h1>Activity this hour</h1>
-                <div class="flex">
-                    <div class="mr-4">Graph 1</div>
-                    <div class="mr-4">Graph 2</div>
-                    <div>Graph 3</div>
+            <!-- Second set of divs -->
+            <div class="mt-4">
+                <div>
+                    <h1>Activity this hour</h1>
+                    <div class="flex">
+                        <div class="mr-4">Graph 1</div>
+                        <div class="mr-4">Graph 2</div>
+                        <div>Graph 3</div>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <!-- Third set of divs -->
-        <div class="mt-4">
-            <div>
-                <h1>Historical stats</h1>
-                <div class="grid grid-cols-2 gap-4">
-                    <div>Graph 1</div>
-                    <div>Graph 2</div>
+            <!-- Third set of divs -->
+            <div class="mt-4">
+                <div>
+                    <h1>Historical stats</h1>
+                    <div class="grid grid-cols-2 gap-4">
+                        <div>Graph 1</div>
+                        <div>Graph 2</div>
+                    </div>
                 </div>
             </div>
         </div>
