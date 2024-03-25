5a7cdf1efbf0983fca8e2286ae2722a193bea039
Dylan Silva <c21088964@cardiff.ac.uk>
2024-03-25 14:28:48 +0000

    added dashboard page with boiler plate code

---
+++ pages/dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div><h1>info 1</h1></div>
+    <div>
+        <div>
+            <h1>Activity this hour</h1>
+        </div>
+    </div>
+    <div>
+        <div>
+            <h1>Historical stats</h1>
+        </div>
+    </div>
+</template>
