e07636103990f4b58431120a9a81c52b1042ec6b
Dylan Silva <c21088964@cardiff.ac.uk>
2024-03-25 14:32:36 +0000

    Add graphs pos to dashboard.vue

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,13 +1,25 @@
 <template>
-    <div><h1>info 1</h1></div>
+    <div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+    </div>
     <div>
         <div>
             <h1>Activity this hour</h1>
+            <div>Graph 1</div>
+            <div>Graph 2</div>
+            <div>Graph 3</div>
         </div>
     </div>
     <div>
         <div>
             <h1>Historical stats</h1>
+            <div>Graph 1</div>
+            <div>Graph 2</div>
         </div>
     </div>
 </template>
