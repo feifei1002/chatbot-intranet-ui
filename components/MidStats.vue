bd59cb7082750d2be1d6bc81008756693c4b48d6
c21088964 <silvad1@cardiff.ac.uk>
2024-04-01 19:07:04 +0100

    added component for middle stats and graphs for clarity

---
+++ components/MidStats.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="relative mr-8 size-64 grow rounded bg-chatbot-white p-12">
+      <span class="absolute left-0 top-0 inline-block w-full rounded-t bg-gray-300 py-2 text-xs font-semibold uppercase text-black">
+        {{ statTitle }}
+      </span>
+      <!-- Use slot to include the LineChart component -->
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+    statTitle: {
+      type: String,
+      required: true
+    }
+  });
+  </script>
+  
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,13 +1,21 @@
 <template>
-    <div class="flex h-screen justify-center   bg-chatbot-black">
+    <div class="flex h-screen justify-center bg-chatbot-black">
         <!-- All content goes here -->
         <div>
             <div class="mt-10 flex text-black">
-                <TopStats statTitle="Conversations" statTimerange="Last Minute" statValue=69></TopStats>
-                <TopStats statTitle="Conversations" statTimerange="Average over last hour" statValue=69></TopStats>
-                <TopStats statTitle="Authenicated User Messages" statTimerange="Average over last 24h" statValue=69></TopStats>
-                <TopStats statTitle="Unauthenicated User Messages" statTimerange="Average over last 24h" statValue=69></TopStats>
-                <TopStats statTitle="Message count" statTimerange="Sent+Received last 24h" statValue=69></TopStats>
+                <TopStats statTitle="Conversations" statTimerange="Last Minute" statValue="69"></TopStats>
+                <TopStats statTitle="Conversations" statTimerange="Average over last hour" statValue="69"></TopStats>
+                <TopStats
+                    statTitle="Authenicated User Messages"
+                    statTimerange="Average over last 24h"
+                    statValue="69"
+                ></TopStats>
+                <TopStats
+                    statTitle="Unauthenicated User Messages"
+                    statTimerange="Average over last 24h"
+                    statValue="69"
+                ></TopStats>
+                <TopStats statTitle="Message count" statTimerange="Sent+Received last 24h" statValue="69"></TopStats>
             </div>
 
             <!-- Second set of divs -->
@@ -15,27 +23,15 @@
                 <div>
                     <h1 class="font-mono text-xl">ACTIVITY THIS HOUR</h1>
                     <div class="flex">
-                        <div class="relative mr-8 size-64 grow rounded bg-chatbot-white p-12">
-                            <span
-                                class="absolute left-0 top-0 inline-block w-full rounded-t bg-gray-300 py-2 text-xs font-semibold uppercase text-black"
-                                >Active conversations</span
-                            >
+                        <MidStats statTitle="Active Conversations">
                             <LineChart />
-                        </div>
-                        <div class="relative mr-8 size-64 grow rounded bg-chatbot-white p-12">
-                            <span
-                                class="absolute left-0 top-0 inline-block w-full rounded-t bg-gray-300 py-2 text-xs font-semibold uppercase text-black"
-                                >Message count</span
-                            >
+                        </MidStats>
+                        <MidStats statTitle="Message Count">
                             <LineChart />
-                        </div>
-                        <div class="relative size-64 grow rounded bg-chatbot-white p-12">
-                            <span
-                                class="absolute left-0 top-0 inline-block w-full rounded-t bg-gray-300 py-2 text-xs font-semibold uppercase text-black"
-                                >Message direction last hour</span
-                            >
+                        </MidStats>
+                        <MidStats statTitle="Tool Usage">
                             <PieChart />
-                        </div>
+                        </MidStats>
                     </div>
                 </div>
             </div>
@@ -67,5 +63,7 @@
 </template>
 
 <script>
-import TopStats from "@/components/TopStats.vue"
+import TopStats from "@/components/TopStats.vue";
+import MidStats from "@/components/MidStats.vue";
+import PieChart from "~/components/PieChart.vue";
 </script>
