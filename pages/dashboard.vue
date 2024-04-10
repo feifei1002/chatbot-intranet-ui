457e728060affccd525d21a9468e319b6cc5f230
c21088964 <silvad1@cardiff.ac.uk>
2024-04-10 03:43:22 +0100

    lint fix with cmd

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -34,7 +34,7 @@
             <!-- Second set of divs -->
             <div class="mt-10">
                 <div>
-                    <h1 class="font-mono text-xl uppercase">{{ $t('dashboard.activityThisHour') }}</h1>
+                    <h1 class="font-mono text-xl uppercase">{{ $t("dashboard.activityThisHour") }}</h1>
                     <div class="flex">
                         <MidStats :stat-title="$t('dashboard.activeConversations')">
                             <LineChart />
@@ -52,7 +52,7 @@
             <!-- Third set of divs -->
             <div class="mt-10">
                 <div>
-                    <h1 class="font-mono text-xl uppercase">{{ $t('dashboard.historicalStats') }}</h1>
+                    <h1 class="font-mono text-xl uppercase">{{ $t("dashboard.historicalStats") }}</h1>
                     <div class="flex">
                         <BottomStats :stat-title="$t('dashboard.numberOfConv24h')">
                             <LineChart />
