d12735b999fb8e25a385b2026600683fcbd1f023
c21088964 <silvad1@cardiff.ac.uk>
2024-04-10 03:26:17 +0100

    Refactor language translations in en-GB.json, ko.json, and cy.json

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -3,33 +3,46 @@
         <!-- All content goes here -->
         <div>
             <div class="mt-10 flex text-black">
-                <TopStats stat-title="Conversations" stat-timerange="Last Minute" stat-value="69"> </TopStats>
-                <TopStats stat-title="Conversations" stat-timerange="Average over last hour" stat-value="69"></TopStats>
                 <TopStats
-                    stat-title="Authenicated User Messages"
-                    stat-timerange="Average over last 24h"
+                    :stat-title="$t('dashboard.conversations')"
+                    :stat-timerange="$t('dashboard.lastMinute')"
+                    stat-value="69"
+                >
+                </TopStats>
+                <TopStats
+                    :stat-title="$t('dashboard.conversations')"
+                    :stat-timerange="$t('dashboard.avgLasthour')"
                     stat-value="69"
                 ></TopStats>
                 <TopStats
-                    stat-title="Unauthenicated User Messages"
-                    stat-timerange="Average over last 24h"
+                    :stat-title="$t('dashboard.authUserMessage')"
+                    :stat-timerange="$t('dashboard.avg24hr')"
                     stat-value="69"
                 ></TopStats>
-                <TopStats stat-title="Message count" stat-timerange="Sent+Received last 24h" stat-value="69"></TopStats>
+                <TopStats
+                    :stat-title="$t('dashboard.unauthUserMessage')"
+                    :stat-timerange="$t('dashboard.avg24hr')"
+                    stat-value="69"
+                ></TopStats>
+                <TopStats
+                    :stat-title="$t('dashboard.msgCount')"
+                    :stat-timerange="$t('dashboard.sentReceived24h')"
+                    stat-value="69"
+                ></TopStats>
             </div>
 
             <!-- Second set of divs -->
             <div class="mt-10">
                 <div>
-                    <h1 class="font-mono text-xl">ACTIVITY THIS HOUR</h1>
+                    <h1 class="font-mono text-xl uppercase">{{ $t('dashboard.activityThisHour') }}</h1>
                     <div class="flex">
-                        <MidStats stat-title="Active Conversations">
+                        <MidStats :stat-title="$t('dashboard.activeConversations')">
                             <LineChart />
                         </MidStats>
-                        <MidStats stat-title="Message Count">
+                        <MidStats :stat-title="$t('dashboard.msgCount')">
                             <LineChart />
                         </MidStats>
-                        <MidStats stat-title="Tool Usage">
+                        <MidStats :stat-title="$t('dashboard.toolUsage')">
                             <PieChart />
                         </MidStats>
                     </div>
@@ -39,12 +52,12 @@
             <!-- Third set of divs -->
             <div class="mt-10">
                 <div>
-                    <h1 class="font-mono text-xl">HISTORICAL STATS</h1>
+                    <h1 class="font-mono text-xl uppercase">{{ $t('dashboard.historicalStats') }}</h1>
                     <div class="flex">
-                        <BottomStats stat-title="Number of conversations (24hr)">
+                        <BottomStats :stat-title="$t('dashboard.numberOfConv24h')">
                             <LineChart />
                         </BottomStats>
-                        <BottomStats stat-title="Number of conversations (7d)">
+                        <BottomStats :stat-title="$t('dashboard.numberOfConv7d')">
                             <LineChart />
                         </BottomStats>
                     </div>
@@ -53,14 +66,3 @@
         </div>
     </div>
 </template>
-
-<script setup>
-// Utilise the useAuth hook to retrieve the authentication data.
-const { data } = useAuth();
-
-// Check if the 'admin' property exists and has a truthy value in the 'data.value' object.
-if (!data.value?.admin) {
-    // If the 'admin' property is missing, undefined, or false, redirect the user to the homepage.
-    navigateTo("/");
-}
-</script>
