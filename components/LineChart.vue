ec693e708a6eb9687691d080860adc0ba72a302a
c21088964 <silvad1@cardiff.ac.uk>
2024-04-11 16:25:25 +0100

    fixes

--- components/LineChart.vue
+++ components/LineChart.vue
@@ -14,9 +14,25 @@ import {
     Tooltip,
     Legend,
 } from "chart.js";
-import * as LineChartConfig from "../config/LineChartConfig";
+// import * as LineChartConfig from "../config/LineChartConfig";
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
-const { data, options } = LineChartConfig;
+const data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "Data One",
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+            data: [65, 59, 80, 81, 56, 55, 40],
+        },
+    ],
+};
+
+const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+};
 </script>
