f2e8326c398afb5ffdfba49281a01383f7d3c523
c21088964 <silvad1@cardiff.ac.uk>
2024-04-11 18:19:56 +0100

    lint fixes and default options for linechart

--- components/LineChart.vue
+++ components/LineChart.vue
@@ -4,6 +4,7 @@
 
 <script setup>
 import { Line as LineChart } from "vue-chartjs";
+import { defineProps } from "vue";
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -14,25 +15,20 @@ import {
     Tooltip,
     Legend,
 } from "chart.js";
-// import * as LineChartConfig from "../config/LineChartConfig";
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
-const data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "Data One",
-            backgroundColor: "rgba(255, 99, 132, 0.2)",
-            borderColor: "rgba(255, 99, 132, 1)",
-            borderWidth: 1,
-            data: [65, 59, 80, 81, 56, 55, 40],
-        },
-    ],
-};
-
-const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-};
+const props = defineProps({
+    data: {
+        type: Object,
+        required: true,
+    },
+    options: {
+        type: Object,
+        default: () => ({
+            responsive: true,
+            maintainAspectRatio: false,
+        }),
+    },
+});
 </script>
