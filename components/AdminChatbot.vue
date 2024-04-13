a55847cceed918d8c9a14c0772126479a9b73cd4
Fei Liu <LiuF29@cardiff.ac.uk>
2024-04-14 02:15:06 +0700

    fixed spelling

--- components/AdminChatbot.vue
+++ components/AdminChatbot.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="flex justify-center text-2xl font-bold">
-        <h2>Admin Chabot</h2>
+        <h2>Admin Chatbot</h2>
     </div>
-    <div class="flex h-3/5 w-full flex-col bg-chatbot-white px-1 pb-1 pt-4">
+    <div class="flex h-3/5 w-full flex-col bg-chatbot-white px-1 pb-1 pt-4 dark:bg-chatbot-font">
         <div class="flex h-full flex-col overflow-y-scroll">
             <div
                 v-for="(message, index) in chatMessages"
                 :key="index"
                 class="mb-2 max-w-xl text-wrap break-words rounded p-1"
                 :class="{
-                    'mr-1 self-end bg-amaranth-300 text-right text-black': message.role === 'user',
+                    'mr-1 self-end bg-amaranth-300 text-right text-black dark:bg-amaranth-800 dark:text-white':
+                        message.role === 'user',
                     'self-start bg-gray-300 text-left text-black': message.role === 'assistant',
                 }"
             >
@@ -40,14 +41,14 @@
             <textarea
                 v-model="adminQuestion"
                 :placeholder="$t('chatbot.message')"
-                class="h-fit min-h-20 w-full rounded-md bg-amaranth-100 p-2 pr-32 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-amaranth-500"
+                class="h-fit min-h-20 w-full rounded-md bg-amaranth-100 p-2 pr-32 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-amaranth-500 dark:bg-chatbot-black dark:text-white dark:focus:text-white dark:focus:ring-chatbot-black"
                 style="color: rgb(6, 5, 5)"
                 @keydown.enter="handleShiftEnter"
             ></textarea>
             <div class="absolute right-2 top-1/2 flex -translate-y-1/2 items-center">
                 <button
                     v-t="'chatbot.send'"
-                    class="rounded-md bg-amaranth-500 px-4 py-2 text-white hover:bg-amaranth-600 focus:outline-none focus:ring-2 focus:ring-amaranth-500 focus:ring-offset-2"
+                    class="rounded-md bg-amaranth-500 px-4 py-2 text-white hover:bg-amaranth-600 focus:outline-none focus:ring-2 focus:ring-amaranth-500 focus:ring-offset-2 dark:bg-chatbot-red dark:text-white dark:hover:text-black"
                     :disabled="generating"
                     @click="sendQuestion"
                 />
