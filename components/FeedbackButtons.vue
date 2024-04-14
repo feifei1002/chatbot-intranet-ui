ff477b5f9e9f47f17a9045b0e58ad2e5f84deecc
eye man ^^ <alia64@cardiff.ac.uk>
2024-04-14 18:58:16 +0100

    UModal + UCard

--- components/FeedbackButtons.vue
+++ components/FeedbackButtons.vue
@@ -2,6 +2,7 @@
 const { token } = useAuth();
 const config = useRuntimeConfig();
 const textBoxShown = ref(false);
+const submitted = ref(false);
 const userInput = defineModel('userInput')
 const props = defineProps({
     id: {
@@ -16,11 +17,11 @@ const props = defineProps({
 
 function showInputBox() {
     // Confirms user is signed in order to submit feedback
-    if (token.value) {
-        textBoxShown.value = true;
-    } else {
-        alert("Please sign in to submit feedback");
-    }
+    // if (token.value) {
+    textBoxShown.value = true;
+    //   } else {
+    //       alert("Please sign in to submit feedback");
+    //  }
 }
 
 function close() {
@@ -47,31 +48,32 @@ async function sendFeedback(isPositive, userInput) {
         // Handle the error as needed, such as showing an error message to the user
     }
     close()
+    submitted.value = true;
 }
 
 </script>
 <template>
     <div class="flex justify-around">
-        <UButton icon="i-heroicons-arrows-up-down" size="sm" color="" variant="solid" :trailing="false"
+        <UButton icon="i-heroicons-arrows-up-down" :disabled="submitted" size="sm" color="" variant="solid" :trailing="false"
             @click="showInputBox" />
     </div>
 
-    <div v-show="textBoxShown" class="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-        <div class="h-2/4 w-2/6 bg-neutral-300 flex flex-col items-center">
-            <div class="text-xl my-6 underline">Feedback Form</div>
-            <div> "{{ props.content }}" </div>
-            <textarea v-model="userInput" placeholder="Please provide feedback on the message above (250 chars)"
-                maxlength="250" class="my-6 h-1/4 w-3/4 text-wrap"></textarea>
-            <div class="my-4">
-                <UButton icon="i-heroicons-hand-thumb-up" size="sm" color="green" variant="solid"
-                    label="Send as Positive Feedback" :trailing="false" @click="sendFeedback(true, userInput)" />
-                <UButton icon="i-heroicons-hand-thumb-down" size="sm" color="red" variant="solid"
-                    label="Send as Negative Feedback" :trailing="false" @click="sendFeedback(false, userInput)" />
+    <UModal v-model="textBoxShown">
+        <UCard background="bg-white dark:bg-gray-900">
+            <div class="w-full h-full flex flex-col items-center justify-center">
+                    <div class="text-xl my-6">Feedback Form</div>
+                <div class="text-wrap"> "{{ props.content }}" </div>
+                <UTextarea v-model="userInput" placeholder="Please provide feedback on the message above (250 chars)"
+                    maxlength="250" class="my-6 h-1/4 w-3/4" />
+                <div class="my-4">
+                    <UButton icon="i-heroicons-hand-thumb-up" size="sm" color="green" variant="solid"
+                        label="Send as Positive Feedback" :trailing="false" @click="sendFeedback(true, userInput)" />
+                    <UButton icon="i-heroicons-hand-thumb-down" size="sm" color="red" variant="solid"
+                        label="Send as Negative Feedback" :trailing="false" @click="sendFeedback(false, userInput)" />
+                </div>
+                <UButton icon="i-heroicons-x-mark" size="sm" color="gray" variant="solid" :trailing="false" label="Close"
+                    @click="close()" />
             </div>
-            <UButton icon="i-heroicons-x-mark" size="sm" color="" variant="solid" :trailing="false" label="Close"
-                @click="close()" />
-        </div>
-    </div>
-
-
+        </UCard>
+    </UModal>
 </template>
