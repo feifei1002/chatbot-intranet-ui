b1de4c02b971123176881b3a4120a5d666a36fc1
eye man ^^ <alia64@cardiff.ac.uk>
2024-04-14 01:24:28 +0100

    uses payload

--- components/FeedbackButtons.vue
+++ components/FeedbackButtons.vue
@@ -16,31 +16,32 @@ const props = defineProps({
 
 function showInputBox() {
     // Confirms user is signed in order to submit feedback
-      if (token.value) {
-    textBoxShown.value = true;
-       } else {
-           alert("Please sign in to submit feedback");
-       }
+    if (token.value) {
+        textBoxShown.value = true;
+    } else {
+        alert("Please sign in to submit feedback");
+    }
 }
 
-function close(){
+function close() {
     textBoxShown.value = false;
 }
 
 async function sendFeedback(isPositive, userInput) {
+    const payload = {
+        message: props.content,
+        positive: isPositive,
+        feedback: userInput
+    }
     try {
         await fetch(`${config.public.apiURL}/feedback`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({
-                message: props.content,
-                positive: isPositive,
-                feedback: userInput
-            }),
+            body: JSON.stringify(payload),
         },
-    );
+        );
     } catch (error) {
         console.error('Error:', error);
         // Handle the error as needed, such as showing an error message to the user
@@ -59,16 +60,16 @@ async function sendFeedback(isPositive, userInput) {
         <div class="h-2/4 w-2/6 bg-neutral-300 flex flex-col items-center">
             <div class="text-xl my-6 underline">Feedback Form</div>
             <div> "{{ props.content }}" </div>
-            <textarea v-model="userInput" placeholder="Please provide feedback on the message above (250 chars)" maxlength="250"
-                class="my-6 h-1/4 w-3/4 text-wrap"></textarea>
+            <textarea v-model="userInput" placeholder="Please provide feedback on the message above (250 chars)"
+                maxlength="250" class="my-6 h-1/4 w-3/4 text-wrap"></textarea>
             <div class="my-4">
                 <UButton icon="i-heroicons-hand-thumb-up" size="sm" color="green" variant="solid"
                     label="Send as Positive Feedback" :trailing="false" @click="sendFeedback(true, userInput)" />
                 <UButton icon="i-heroicons-hand-thumb-down" size="sm" color="red" variant="solid"
                     label="Send as Negative Feedback" :trailing="false" @click="sendFeedback(false, userInput)" />
             </div>
-            <UButton icon="i-heroicons-x-mark" size="sm" color="" variant="solid" :trailing="false"
-                label="Close" @click="close()" />
+            <UButton icon="i-heroicons-x-mark" size="sm" color="" variant="solid" :trailing="false" label="Close"
+                @click="close()" />
         </div>
     </div>
 
