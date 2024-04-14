0ad751b0c5cc65ee6b1ad5c7c12c8153224d5e67
Alexander Tarling <tarlinga@cardiff.ac.uk>
2024-04-14 15:28:44 +0100

    Fixed merge conflict in AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,5 @@
 <script setup>
-const { status, signOut } = useAuth();
+const { status,signOut } = useAuth();
 const authStatus = ref(status.value === "authenticated");
 
 watch(status, newStatus => {
@@ -10,6 +10,7 @@ const { locale } = useI18n({ useScope: "global" });
 const cookieLocale = useCookie("locale");
 
 locale.value = cookieLocale.value ?? "en";
+
 
 watch(locale, () => {
     cookieLocale.value = locale.value;
@@ -23,53 +24,55 @@ const logout = async () => {
 </script>
 
 <template>
-    <nav class="bg-chatbot-white p-6 drop-shadow-lg">
+    <nav class="bg-chatbot-white p-6 drop-shadow-lg dark:bg-chatbot-black">
         <div class="container flex items-center">
             <NuxtImg preload src="/img/logo.png" width="60" />
-            <NuxtLink v-t="'titles.chatbot'" to="/" class="w-3/4 pl-4 text-xl font-bold text-chatbot-font" />
-            <div class="absolute right-0 flex w-1/4 justify-between pr-6">
+            <NuxtLink
+                v-t="'titles.chatbot'"
+                to="/"
+                class="w-3/4 pl-4 text-xl font-bold text-chatbot-font dark:text-chatbot-white"
+            />
+            <div class="absolute right-0 flex w-1/3 justify-between pr-6">
                 <form class="flex">
                     <select
                         id="locale-select"
                         v-model="$i18n.locale"
-                        class="border-2 border-solid border-black bg-white text-chatbot-red"
+                        class="border-2 border-solid border-black bg-white text-chatbot-red dark:border-chatbot-white dark:bg-chatbot-black dark:text-chatbot-white"
                     >
                         <option value="en">en</option>
                         <option value="cy">cy</option>
                         <option value="ko">ko</option>
                     </select>
                 </form>
+                <DarkMode></DarkMode>
                 <NuxtLinkLocale
                     v-t="'titles.home'"
                     to="/"
-                    class="text-xl font-semibold text-chatbot-font"
+                    class="text-xl font-semibold text-chatbot-font dark:text-chatbot-white"
                     exact-active-class="!text-chatbot-red"
                 />
-                >
-                <!-- sign in shows when not signed in -->
                 <NuxtLinkLocale
                     v-if="!authStatus"
                     v-t="'titles.signin'"
                     to="/signin"
-                    class="text-xl font-semibold text-chatbot-font"
+                    class="text-xl font-semibold text-chatbot-font dark:text-chatbot-white"
                     exact-active-class="!text-chatbot-red"
                 />
-                >
                 <!-- sign out shows when signed in -->
                 <NuxtLinkLocale
                     v-if="authStatus"
                     v-t="'titles.signout'"
-                    class="cursor-pointer text-xl font-semibold text-chatbot-font hover:text-chatbot-red"
+                    class="cursor-pointer text-xl font-semibold text-chatbot-font dark:text-chatbot-white dark:hover:text-chatbot-red hover:text-chatbot-red"
                     @click="logout()"
                 />
-                >
                 <NuxtLinkLocale
                     to="/chat"
-                    class="text-xl font-semibold text-chatbot-font"
+                    class="text-xl font-semibold text-chatbot-font dark:text-chatbot-white"
                     exact-active-class="!text-chatbot-red"
-                    >{{ $t("titles.trychatbot") }}
+                >{{ $t("titles.trychatbot") }}
                     <UIcon name="i-heroicons-arrow-up-right" />
                 </NuxtLinkLocale>
+
             </div>
         </div>
     </nav>
