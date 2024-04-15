798456343a0f7f8977c973b58e6b17f73431379d
Alexander Tarling <tarlinga@cardiff.ac.uk>
2024-04-15 18:18:23 +0100

    Added colours to each graph and fixed up code formatting and added comments

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex justify-center bg-chatbot-black">
-        <!-- All content goes here -->
         <div>
+            <!-- analytics for recent conversations and messages -->
             <div class="mt-10 flex text-black">
                 <TopStats
                     :stat-title="$t('dashboard.conversations')"
@@ -31,34 +31,34 @@
                 ></TopStats>
             </div>
 
-            <!-- Second set of divs -->
+            <!-- line charts for conversations over 1 day and 1 hour, and tool usage pie chart -->
             <div class="mt-10">
                 <div>
                     <h1 class="font-mono text-xl uppercase">{{ $t("dashboard.recentActivity") }}</h1>
                     <div class="flex">
                         <MidStats :stat-title="$t('dashboard.numberOfConv24h')">
-                            <LineChart :data="convDayDataLine" :options="tsOptionsHour" />
+                            <LineChart :data="convDayDataSet" :options="hourChartOptions" />
                         </MidStats>
                         <MidStats :stat-title="$t('dashboard.numberOfConv1h')">
-                            <LineChart :data="convHourDataLine" :options="tsOptionsMinute" />
+                            <LineChart :data="convHourDataSet" :options="minuteChartOptions" />
                         </MidStats>
                         <MidStats :stat-title="$t('dashboard.toolUsage')">
-                            <PieChart :data="toolsPieChart" />
+                            <PieChart :data="toolsPieChartData" />
                         </MidStats>
                     </div>
                 </div>
             </div>
 
-            <!-- Third set of divs -->
+            <!-- line charts for conversations and messages over 7 days -->
             <div class="mt-10">
                 <div>
                     <h1 class="font-mono text-xl uppercase">{{ $t("dashboard.historicalStats") }}</h1>
                     <div class="flex">
                         <BottomStats :stat-title="$t('dashboard.numberOfMessages7d')">
-                            <LineChart :data="messages7DataLine" :options="tsOptionsDay" />
+                            <LineChart :data="messages7DataSet" :options="dayChartOptions" />
                         </BottomStats>
                         <BottomStats :stat-title="$t('dashboard.numberOfConv7d')">
-                            <LineChart :data="conversations7DataLine" :options="tsOptionsDay" />
+                            <LineChart :data="conversationsSevenDataSet" :options="dayChartOptions" />
                         </BottomStats>
                     </div>
                 </div>
@@ -68,9 +68,11 @@
 </template>
 
 <script setup>
-definePageMeta({ middleware: "auth" });
-
+const config = useRuntimeConfig();
+const { token } = useAuth();
 const { data: authData } = useAuth();
+
+definePageMeta({ middleware: "auth" });
 
 // Check if the 'admin' property exists and has a truthy value in the 'data.value' object.
 if (!authData.value?.admin) {
@@ -78,9 +80,7 @@ if (!authData.value?.admin) {
     navigateTo("/");
 }
 
-const config = useRuntimeConfig();
-const { token } = useAuth();
-
+// fetch analytics data
 const { data: jsonData } = await useFetch(`${config.public.apiURL}/admin/query`, {
     method: "GET",
     headers: {
@@ -89,15 +89,100 @@ const { data: jsonData } = await useFetch(`${config.public.apiURL}/admin/query`,
     },
 });
 
-const conversationsSevenData = jsonData.value?.conversations_7d_chart || [];
-const messagesSevenData = jsonData.value?.messages_7d_chart || [];
+// format pie chart labels from for example: 'search_uni_website' to 'Search Uni Website'
+const formatLabel = str => {
+    return str.replace(/_/g, " ").replace(/\b\w/g, match => match.toUpperCase());
+};
 
-const conversationsDayData = jsonData.value?.conversations_1d_chart || [];
-const conversationsHourData = jsonData.value?.conversations_1h_chart || [];
+// analytics data
+// first row of graph data
+const conversationsDayChart = jsonData.value?.conversations_1d_chart || [];
+const conversationsHourChart = jsonData.value?.conversations_1h_chart || [];
 
+// pie chart data
 const toolsData = jsonData.value?.tools;
+// format labels for chart
+const labels = Object.keys(toolsData);
+formatLabel(toolsData.toString());
+const formattedLabels = labels.map(formatLabel);
 
-const tsOptionsDay = {
+// second row of graph data
+const conversations7DChart = jsonData.value?.conversations_7d_chart || [];
+const messages7DChart = jsonData.value?.messages_7d_chart || [];
+
+// data set for conversations over the most recent 7 days
+const conversationsSevenDataSet = {
+    datasets: [
+        {
+            label: "Conversations",
+            data: conversations7DChart.map(item => ({
+                x: new Date(item.x),
+                y: item.y,
+            })),
+            borderColor: "#1d65a6",
+            tension: 0.1,
+        },
+    ],
+};
+
+// data set for messages over the most recent 7 days
+const messages7DataSet = {
+    datasets: [
+        {
+            label: "Messages",
+            data: messages7DChart.map(item => ({
+                x: new Date(item.x),
+                y: item.y,
+            })),
+            borderColor: "#00743f",
+            tension: 0.1,
+        },
+    ],
+};
+
+// data set for conversations over the past day
+const convDayDataSet = {
+    datasets: [
+        {
+            label: "Conversations",
+            data: conversationsDayChart.map(item => ({
+                x: new Date(item.x),
+                y: item.y,
+            })),
+            borderColor: "#e52918",
+            tension: 0.1,
+        },
+    ],
+};
+
+// data set for conversations over the past hour
+const convHourDataSet = {
+    datasets: [
+        {
+            label: "Conversations",
+            data: conversationsHourChart.map(item => ({
+                x: new Date(item.x),
+                y: item.y,
+            })),
+            borderColor: "#f25117",
+            tension: 0.1,
+        },
+    ],
+};
+
+// data for the pie chart showing tools
+const toolsPieChartData = {
+    labels: formattedLabels,
+    datasets: [
+        {
+            backgroundColor: ["#192e5b", "#1d65a6", "#72a2c0", "#00743f", "#f2a104", "#f25117"], // colours for each tool
+            data: Object.values(toolsData),
+        },
+    ],
+};
+
+// options for data with unit in days
+const dayChartOptions = {
     maintainAspectRatio: false,
     scales: {
         x: {
@@ -111,10 +196,16 @@ const tsOptionsDay = {
                 unit: "day",
             },
         },
+        y: {
+            ticks: {
+                beginAtZero: true,
+            },
+        },
     },
 };
 
-const tsOptionsHour = {
+// options for data with unit in hours
+const hourChartOptions = {
     maintainAspectRatio: false,
     scales: {
         x: {
@@ -128,15 +219,21 @@ const tsOptionsHour = {
                 unit: "hour",
             },
             ticks: {
-                stepSize: 2, // set the step size to 2 hours
+                stepSize: 4, // set the step size to 2 hours
                 maxRotation: 45,
                 minRotation: 30,
+            },
+        },
+        y: {
+            ticks: {
+                beginAtZero: true,
             },
         },
     },
 };
 
-const tsOptionsMinute = {
+// options for data with unit in minutes
+const minuteChartOptions = {
     maintainAspectRatio: false,
     scales: {
         x: {
@@ -150,77 +247,16 @@ const tsOptionsMinute = {
                 unit: "minute",
             },
             ticks: {
-                stepSize: 5, // set the step size to 5 minutes
+                stepSize: 10, // set the step size to 5 minutes
                 maxRotation: 45,
                 minRotation: 30,
             },
         },
+        y: {
+            ticks: {
+                beginAtZero: true,
+            },
+        },
     },
-};
-
-const conversations7DataLine = {
-    datasets: [
-        {
-            label: "Conversations",
-            data: conversationsSevenData.map(item => ({
-                x: new Date(item.x),
-                y: item.y,
-            })),
-            borderColor: "rgb(75, 192, 192)",
-            tension: 0.1,
-        },
-    ],
-};
-
-const messages7DataLine = {
-    datasets: [
-        {
-            label: "Messages",
-            data: messagesSevenData.map(item => ({
-                x: new Date(item.x),
-                y: item.y,
-            })),
-            borderColor: "rgb(75, 192, 192)",
-            tension: 0.1,
-        },
-    ],
-};
-
-const convDayDataLine = {
-    datasets: [
-        {
-            label: "Conversations",
-            data: conversationsDayData.map(item => ({
-                x: new Date(item.x),
-                y: item.y,
-            })),
-            borderColor: "rgb(75, 192, 192)",
-            tension: 0.1,
-        },
-    ],
-};
-
-const convHourDataLine = {
-    datasets: [
-        {
-            label: "Conversations",
-            data: conversationsHourData.map(item => ({
-                x: new Date(item.x),
-                y: item.y,
-            })),
-            borderColor: "rgb(75, 192, 192)",
-            tension: 0.1,
-        },
-    ],
-};
-
-const toolsPieChart = {
-    labels: Object.keys(toolsData),
-    datasets: [
-        {
-            backgroundColor: ["#41B883", "#E46651"], // change so each tool has one, need to find out how many tools
-            data: Object.values(toolsData),
-        },
-    ],
 };
 </script>
