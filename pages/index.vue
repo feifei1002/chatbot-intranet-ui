3e791a2c4316b6672e61133cc6eb285d673d2155
Fei Liu <LiuF29@cardiff.ac.uk>
2024-04-16 05:55:53 +0700

    fixed lint

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="flex h-screen w-screen flex-col md:flex-row">
-        <div class="flex h-1/2 w-screen flex-1 items-center justify-center bg-chatbot-black md:h-screen md:w-1/2 dark:bg-chatbot-font">
+        <div
+            class="flex h-1/2 w-screen flex-1 items-center justify-center bg-chatbot-black md:h-screen md:w-1/2 dark:bg-chatbot-font"
+        >
             <p v-t="'index.about'" class="text-balance text-center font-mono text-4xl font-bold text-white"></p>
         </div>
-        <div class="flex h-1/2 w-screen items-center justify-center bg-chatbot-red md:h-screen md:w-1/2 dark:bg-chatbot-dark-red">
+        <div
+            class="flex h-1/2 w-screen items-center justify-center bg-chatbot-red md:h-screen md:w-1/2 dark:bg-chatbot-dark-red"
+        >
             <div class="flex items-center justify-center">
                 <!-- Can adjust the width of the image  if necessary-->
                 <NuxtImg src="/img/landingImage.png" />
